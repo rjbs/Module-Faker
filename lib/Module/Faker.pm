b6cb5ba40871fba33bbc5b4ff597065f2fa09074
Ricardo SIGNES <rjbs@cpan.org>
2008-03-15 22:32:40 -0400

    some docs

--- lib/Module/Faker.pm
+++ lib/Module/Faker.pm
@@ -25,10 +25,47 @@ use File::Next ();
     dest   => './will-contain-tarballs',
   });
 
+=head2 DESCRIPTION
+
+Module::Faker is a tool for building fake CPAN modules and, perhaps more
+importantly, fake CPAN distributions.  These are useful for running tools that
+operate against CPAN distributions without having to use real CPAN
+distributions.  This is much more useful when testing an entire CPAN instance,
+rather than a single distribution, for which see L<CPAN::Faker|CPAN::Faker>.
+
+=head1 METHODS
+
+=head2 make_fakes
+
+  Module::Faker->make_fakes(\%arg);
+
+This method creates a new Module::Faker and builds archives in its destination
+directory for every dist-describing file in its source directory.  See the
+L</new> method below.
+
+=head2 new
+
+  my $faker = Module::Faker->new(\%arg);
+
+This create the new Module::Faker.  All arguments may be accessed later by
+methods of the same name.  Valid arguments are:
+
+  source - the directory in which to find source files
+  dest   - the directory in which to construct dist archives
+
+  dist_class - the class used to fake dists; default: Module::Faker::Dist
+
 =cut
 
 has source => (is => 'ro', required => 1);
 has dest   => (is => 'ro', required => 1);
+
+has dist_class => (
+  is  => 'ro',
+  isa => 'Str',
+  required => 1,
+  default  => sub { 'Module::Faker::Dist' },
+);
 
 sub BUILD {
   my ($self) = @_;
@@ -37,8 +74,10 @@ sub BUILD {
     my $dir = $self->$_;
     Carp::croak "$_ directory does not exist"     unless -e $dir;
     Carp::croak "$_ directory is not a directory" unless -d $dir;
-    Carp::croak "$_ directory is not writeable"   unless -w $dir;
+    Carp::croak "$_ directory is not readable"    unless -r $dir;
   }
+
+  Carp::croak "$_ directory is not writeable" unless -w $self->dest;
 }
 
 sub make_fakes {
@@ -53,8 +92,6 @@ sub make_fakes {
     $dist->make_archive({ dir => $self->dest });
   }
 }
-
-sub dist_class { 'Module::Faker::Dist' }
 
 =head1 COPYRIGHT AND AUTHOR
 
