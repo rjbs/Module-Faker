36e6dcb28309c113640b071f13b2e97f26cca4ab
Ricardo SIGNES <rjbs@cpan.org>
2008-03-19 16:28:19 -0400

    order inner pkgs

--- lib/Module/Faker/Package.pm
+++ lib/Module/Faker/Package.pm
@@ -9,8 +9,17 @@ has name     => (is => 'ro', isa => 'Str', required => 1);
 has version  => (is => 'ro', isa => 'Maybe[Str]');
 has abstract => (is => 'ro', isa => 'Maybe[Str]');
 
-# TODO: default this in from name -- rjbs, 2008-03-12
-has in_file => (is => 'ro', isa => 'Str', required => 1);
+has in_file  => (
+  is       => 'ro',
+  isa      => 'Str',
+  lazy     => 1,
+  default  => sub {
+    my ($self) = @_;
+    my $name = $self->name;
+    $name =~ s{::}{/}g;
+    return "lib/$name";
+  },
+);
 
 subtype 'Module::Faker::Type::Packages'
   => as 'ArrayRef[Module::Faker::Package]';
@@ -20,7 +29,13 @@ coerce 'Module::Faker::Type::Packages'
   => via  {
     my ($href) = @_;
     my @packages;
-    for my $name (keys %$href) {
+
+    my @pkg_names = do {
+      no warnings 'uninitialized';
+      sort { $href->{$a}{Faker}{order} <=> $href->{$b}{Faker}{order} } keys %$href;
+    };
+
+    for my $name (@pkg_names) {
       push @packages, __PACKAGE__->new({
         name    => $name,
         version => $href->{$name}{version},
